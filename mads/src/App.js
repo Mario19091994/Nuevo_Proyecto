05f40c73a0d394aef4e282e8572b7516bbdaad6f
Mario19091994 <132527498+Mario19091994@users.noreply.github.com>
2023-09-29 19:22:47 -0600

    Agregados recientes

--- mads/src/App.js
+++ mads/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import MyButton from './functions/bottom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Todo se derrumbo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bienvenido soldado</h1>
+      <MyButton />
     </div>
   );
 }
