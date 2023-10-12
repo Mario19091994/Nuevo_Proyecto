c23d311d6ed4e47c49a259faad6f19a77d385ea9
Mario19091994 <132527498+Mario19091994@users.noreply.github.com>
2023-10-12 01:23:26 -0600

    Nuevos Cambios

---
+++ mads/src/functions/WelcomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './WelcomePage.css'; // Importa los estilos
+
+function WelcomePage() {
+  return (
+    <div className="welcome-container">
+      <h1>Bienvenido</h1>
+      <p>¡Has iniciado sesión con éxito!</p>
+    </div>
+  );
+}
+
+export default WelcomePage;
--- mads/src/functions/bottom.js
+++ mads/src/functions/bottom.js
@@ -1,52 +1,52 @@
 import React, { useState } from 'react';
 import './Login.css';
+import WelcomePage from './WelcomePage'; // Importa la página de bienvenida
+
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [loginSuccess, setLoginSuccess] = useState(false);
+  const [redirectToWelcome, setRedirectToWelcome] = useState(false);
 
   const handleLogin = () => {
-    // Aquí puedes simular la lógica de autenticación (por ejemplo, comparando credenciales ficticias).
-    // Para fines de este ejemplo, asumiremos que las credenciales válidas son 'correo@example.com' y 'contraseña'.
-
-    if (email === 'correo@example.com' && password === 'contraseña') {
-      setLoginSuccess(true);
-    } else {
-      setLoginSuccess(false);
+    if (email.trim() !== '' && password.trim() !== '') {
+      setRedirectToWelcome(true); // Activa la redirección si ambos campos no están vacíos
     }
   };
+
+  if (redirectToWelcome) {
+    return <WelcomePage />;
+  }
 
   return (
     <div className="login-container">
       <div className="login-box">
-      <h1>Iniciar sesión</h1>
-      <form>
-        <div>
-          <label>Correo electrónico:</label>
-          <input
-            type="text"
-            placeholder="Ingrese su correo electrónico"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Contraseña:</label>
-          <input
-            type="password"
-            placeholder="Ingrese su contraseña"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <hr></hr>
-        <button onClick={handleLogin}>Iniciar Sesión</button>
-      </form>
-      {loginSuccess && <p>¡Inicio de sesión exitoso!</p>}
-      {loginSuccess === false && <p>Inicio de sesión fallido. Verifique su correo electrónico y contraseña.</p>}
-    </div>
+        <h1>Iniciar sesión</h1>
+        <form>
+          <div>
+            <label>Correo electrónico:</label>
+            <input
+              type="text"
+              placeholder="Ingrese su correo electrónico"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Contraseña:</label>
+            <input
+              type="password"
+              placeholder="Ingrese su contraseña"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <hr />
+          <button onClick={handleLogin}>Iniciar Sesión</button>
+        </form>
+      </div>
     </div>
   );
 }
 
 export default Login;
+
