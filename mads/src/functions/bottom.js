e25983b50a666face6b4c0823e32fd7852e62067
Mario19091994 <132527498+Mario19091994@users.noreply.github.com>
2023-10-11 19:37:55 -0600

    Nuevas modificaciones

--- mads/src/functions/bottom.js
+++ mads/src/functions/bottom.js
@@ -1,16 +1,52 @@
-import Button from 'react-bootstrap/Button';
-function  MyButton() {
-    return (
-        <>
-      <Button variant="outline-primary">Primary</Button>{' '}
-      <Button variant="outline-secondary">Secondary</Button>{' '}
-      <Button variant="outline-success">Success</Button>{' '}
-      <Button variant="outline-warning">Warning</Button>{' '}
-      <Button variant="outline-danger">Danger</Button>{' '}
-      <Button variant="outline-info">Info</Button>{' '}
-      <Button variant="outline-light">Light</Button>{' '}
-      <Button variant="outline-dark">Dark</Button>
-      </>
-    );
-  }
-  export default MyButton;
+import React, { useState } from 'react';
+import './Login.css';
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginSuccess, setLoginSuccess] = useState(false);
+
+  const handleLogin = () => {
+    // Aquí puedes simular la lógica de autenticación (por ejemplo, comparando credenciales ficticias).
+    // Para fines de este ejemplo, asumiremos que las credenciales válidas son 'correo@example.com' y 'contraseña'.
+
+    if (email === 'correo@example.com' && password === 'contraseña') {
+      setLoginSuccess(true);
+    } else {
+      setLoginSuccess(false);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-box">
+      <h1>Iniciar sesión</h1>
+      <form>
+        <div>
+          <label>Correo electrónico:</label>
+          <input
+            type="text"
+            placeholder="Ingrese su correo electrónico"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Contraseña:</label>
+          <input
+            type="password"
+            placeholder="Ingrese su contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <hr></hr>
+        <button onClick={handleLogin}>Iniciar Sesión</button>
+      </form>
+      {loginSuccess && <p>¡Inicio de sesión exitoso!</p>}
+      {loginSuccess === false && <p>Inicio de sesión fallido. Verifique su correo electrónico y contraseña.</p>}
+    </div>
+    </div>
+  );
+}
+
+export default Login;
